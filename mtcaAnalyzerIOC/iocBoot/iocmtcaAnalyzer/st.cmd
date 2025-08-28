fe9bd5938304949f4734e9a1163fc67405990420
Michael Brunner <michael.brunner@pndetector.de>
2025-08-28 11:27:54 +0200

    feat(module): Add autosave

--- mtcaAnalyzerIOC/iocBoot/iocmtcaAnalyzer/st.cmd
+++ mtcaAnalyzerIOC/iocBoot/iocmtcaAnalyzer/st.cmd
@@ -25,8 +25,19 @@ dbLoadRecords("db/mtcaChannelTransform.template", "PORT=$(PORT),ADDR=2,P=$(PREFI
 dbLoadRecords("db/mtcaChannelTransform.template", "PORT=$(PORT),ADDR=3,P=$(PREFIX),R=CH,CH=3")
 dbLoadRecords("db/mtcaTransform.template", "PORT=$(PORT),ADDR=3,P=$(PREFIX),R=")
 
+# Autosave
+# Set the directory for save files
+set_savefile_path("$(TOP)/as/save")
+
+# Set the directory for request files  
+set_requestfile_path("$(TOP)/as/req")
+
+#set_pass0_restoreFile("", "")
+set_pass1_restoreFile("auto_settings.sav", "P=$(PREFIX), R=, CH=CH1")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
+# Create monitor sets for your PVs
+create_monitor_set("auto_settings.req", 5, "P=$(PREFIX), R=, CH=CH1")
 
